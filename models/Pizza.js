427f2a0deb455c9457bd987086e9bb000e19cfd0
AndreBlankholm <ablankholm@gmail.com>
2023-03-01 17:29:15 -0600

    added some validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,50 +1,57 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 
 // MAKE a schema for new pizza NOT created
 
 const PizzaSchema = new Schema(
-    {
-      pizzaName: {
-        type: String
-      },
-      createdBy: {
-        type: String
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => dateFormat(createdAtVal)
-      },
-      size: {
-        type: String,
-        default: 'Large'
-      },
-      toppings: [],
-      comments: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'Comment',
-        }
-      ]
+  {
+    pizzaName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-      toJSON: {
-        virtuals: true,
-        getters: true
+    createdBy: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      required: true,
+      enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+      default: 'Large'
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
       },
-      id: false
-    }
-  );
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
 
-
-  
 //VIRTUALS;
 // Virtuals allow you to add virtual properties to a document that aren't stored in the database. They're normally computed values that get evaluated when you try to access their properties.
 
 // This Virtual gets the total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
 // NOW create the Pizza model using the PizzaSchema
